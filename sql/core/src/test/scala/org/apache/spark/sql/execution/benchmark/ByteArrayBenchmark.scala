bc1c61e5f16f519c3079013f871a53f84313e31a
ulysses-you <ulyssesyou18@gmail.com>
2021-10-24 15:45:22 +0800

    benchmark both

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/ByteArrayBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/ByteArrayBenchmark.scala
@@ -20,6 +20,7 @@ package org.apache.spark.sql.execution.benchmark
 import scala.util.Random
 
 import org.apache.spark.benchmark.{Benchmark, BenchmarkBase}
+import org.apache.spark.unsafe.types.ByteArray
 
 /**
  * Benchmark to measure performance for byte array comparisons.
@@ -66,25 +67,35 @@ object ByteArrayBenchmark extends BenchmarkBase {
     val dataLargeSlow = Seq.fill(count)(
       Array.tabulate(512) {i => if (i < 511) 0.toByte else 1.toByte}).toArray
 
-    def compareBinary(data: Array[Array[Byte]]) = { _: Int =>
+    def compareBinary(
+        data: Array[Array[Byte]], compareFunc: (Array[Byte], Array[Byte]) => Int) = { _: Int =>
       var sum = 0L
       for (_ <- 0L until iters) {
         var i = 0
         while (i < count) {
-          sum += compareBinaryOld(data(i), data((i + 1) % count))
+          sum += compareFunc(data(i), data((i + 1) % count))
           i += 1
         }
       }
     }
 
-    val benchmark = new Benchmark("Byte Array compareTo", count * iters, 25, output = output)
-    benchmark.addCase("2-7 byte")(compareBinary(dataTiny))
-    benchmark.addCase("8-16 byte")(compareBinary(dataSmall))
-    benchmark.addCase("16-32 byte")(compareBinary(dataMedium))
-    benchmark.addCase("512-1024 byte")(compareBinary(dataLarge))
-    benchmark.addCase("512 byte slow")(compareBinary(dataLargeSlow))
-    benchmark.addCase("2-7 byte")(compareBinary(dataTiny))
+    val benchmark = new Benchmark("Byte Array compare offHeap", count * iters, 25, output = output)
+    benchmark.addCase("2-7 byte")(compareBinary(dataTiny, ByteArray.compareBinary))
+    benchmark.addCase("8-16 byte")(compareBinary(dataSmall, ByteArray.compareBinary))
+    benchmark.addCase("16-32 byte")(compareBinary(dataMedium, ByteArray.compareBinary))
+    benchmark.addCase("512-1024 byte")(compareBinary(dataLarge, ByteArray.compareBinary))
+    benchmark.addCase("512 byte slow")(compareBinary(dataLargeSlow, ByteArray.compareBinary))
+    benchmark.addCase("2-7 byte")(compareBinary(dataTiny, ByteArray.compareBinary))
     benchmark.run()
+
+    val benchmark2 = new Benchmark("Byte Array compare onHeap", count * iters, 25, output = output)
+    benchmark2.addCase("2-7 byte")(compareBinary(dataTiny, compareBinaryOld))
+    benchmark2.addCase("8-16 byte")(compareBinary(dataSmall, compareBinaryOld))
+    benchmark2.addCase("16-32 byte")(compareBinary(dataMedium, compareBinaryOld))
+    benchmark2.addCase("512-1024 byte")(compareBinary(dataLarge, compareBinaryOld))
+    benchmark2.addCase("512 byte slow")(compareBinary(dataLargeSlow, compareBinaryOld))
+    benchmark2.addCase("2-7 byte")(compareBinary(dataTiny, compareBinaryOld))
+    benchmark2.run()
   }
 
   override def runBenchmarkSuite(mainArgs: Array[String]): Unit = {
